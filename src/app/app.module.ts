68d90d17ffff20a0d2aa23a9e009424c7bb5678a
zouhair <ettarak.zouhair@gmail.com>
2021-07-29 18:34:20 +0200

    Angular - Read Data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ListEmployeeComponent } from './components/employee/list-employee/list-employee.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListEmployeeComponent
   ],
   imports: [
     BrowserModule
