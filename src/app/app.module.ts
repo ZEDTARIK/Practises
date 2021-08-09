bc59af1961361efb07420da0e95a64053f19cdd9
zouhair <ettarak.zouhair@gmail.com>
2021-08-09 21:01:48 +0200

    Angular 7 - Formation add component and routting

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ListEmployeeComponent } from './components/employee/list-employee/list-employee.component';
 import { ListClientComponent } from './components/clients/list-client/list-client.component';
+import { CreateEmployeeComponent } from './employee/create-employee/create-employee.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ListEmployeeComponent,
-    ListClientComponent
+    ListClientComponent,
+    CreateEmployeeComponent
   ],
   imports: [
     BrowserModule
